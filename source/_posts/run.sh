5754be928dbf96b31fbaeaf241674746f4d3078f
lihanyu <1328411791@qq.com>
2025-10-18 16:14:42 +0800

    20251018

---
+++ source/_posts/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+if [ "$EUID" -ne 0 ]; then
+    echo "Swithing to root..."
+    exec sudo $0 "$@"
+fi
+
+apt-get install -y git dkms
+
+cd WSL2-Linux-Kernel-linux-msft-wsl-6.6.36.3
+VERSION=149cbd1
+
+cp -r drivers/hv/dxgkrnl /usr/src/dxgkrnl-$VERSION
+mkdir -p /usr/src/dxgkrnl-$VERSION/inc/{uapi/misc,linux}
+cp include/uapi/misc/d3dkmthk.h /usr/src/dxgkrnl-$VERSION/inc/uapi/misc/d3dkmthk.h
+cp include/linux/hyperv.h /usr/src/dxgkrnl-$VERSION/inc/linux/hyperv_dxgkrnl.h
+sed -i 's/\$(CONFIG_DXGKRNL)/m/' /usr/src/dxgkrnl-$VERSION/Makefile
+sed -i 's#linux/hyperv.h#linux/hyperv_dxgkrnl.h#' /usr/src/dxgkrnl-$VERSION/dxgmodule.c
+echo "EXTRA_CFLAGS=-I\$(PWD)/inc" >> /usr/src/dxgkrnl-$VERSION/Makefile
+
+cat > /usr/src/dxgkrnl-$VERSION/dkms.conf <<EOF
+PACKAGE_NAME="dxgkrnl"
+PACKAGE_VERSION="$VERSION"
+BUILT_MODULE_NAME="dxgkrnl"
+DEST_MODULE_LOCATION="/kernel/drivers/hv/dxgkrnl/"
+AUTOINSTALL="yes"
+EOF
+
+dkms add dxgkrnl/$VERSION
+dkms build dxgkrnl/$VERSION
+dkms install dxgkrnl/$VERSION
